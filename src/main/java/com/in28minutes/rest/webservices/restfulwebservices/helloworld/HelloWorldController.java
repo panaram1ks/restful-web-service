145c8ec37bfdc409ffe8a079a920e50f4699d19b
evgen <parom.evgen@mail.ru>
2023-04-01 10:43:54 +0300

    hello world RestController

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+public class HelloWorldController {
+
+
+//    @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
