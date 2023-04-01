6def015666df6675ebf479187a5f0f08e7e3f235
evgen <parom.evgen@mail.ru>
2023-04-01 11:33:57 +0300

    path parameter, path variable

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
@@ -8,15 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 
-//    @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
+    //    @RequestMapping(path = "/hello-world", method = RequestMethod.GET)
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // /users/{id}/todos/{id} => /users/1/todos/200
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
