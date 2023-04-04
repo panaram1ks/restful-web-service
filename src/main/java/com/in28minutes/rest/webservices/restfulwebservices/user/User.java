3a77118622bda9b459e7bde66a0b3fd87649a7f9
parom <E.Parominskiy@isida.by>
2023-04-04 08:36:44 +0300

    create Post in User with ManyToOne relationship

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,14 +1,17 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity(name = "user_details")
 public class User {
@@ -24,6 +27,10 @@ public class User {
     @Past(message = "Birth Date should be in the past")
 //    @JsonProperty("birth_Date")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user") // user the field in Post describe the relationship
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
